8dc09cde2ad1206eddcd1d7ab3dacaf9f4ceb016
pjcast <pjcast@github.com>
2026-04-04 10:36:00 -0700

    update

--- apps/mixingroom/src/lib/areas/Setlists.svelte
+++ apps/mixingroom/src/lib/areas/Setlists.svelte
@@ -1,7 +1,21 @@
-<script>
+<script lang="ts">
 	import { appState } from "../../State.svelte";
+	import Upload from "./components/setlist/Upload.svelte";
 
 </script>
 
-<p>Setlist</p>
 <button onclick={() => appState.setView('Home')}>Go Home</button>
+
+<section>
+	<div class="song-library">
+		<h3>Setlist</h3>
+
+		<div class="song-list">
+			<p>Upload setlist</p>
+		</div>
+
+		<div class="upload-section">
+			<Upload />
+		</div>
+	</div>
+</section>
--- apps/mixingroom/src/lib/areas/SongLibrary.svelte
+++ apps/mixingroom/src/lib/areas/SongLibrary.svelte
@@ -1,7 +1,10 @@
 <script>
 	import { appState } from "../../State.svelte";
+	import Upload from "./components/setlist/Upload.svelte";
 
 </script>
 
 <p>Song Library</p>
 <button onclick={() => appState.setView('Home')}>Go Home</button>
+
+
