a7245c3a0d5e3a4cbb1001264853b40c882b1893
pjcast <pjcast@github.com>
2026-04-04 10:27:58 -0700

    cleanup

--- apps/mixingroom/src/App.svelte
+++ apps/mixingroom/src/App.svelte
@@ -1,20 +1,20 @@
 <script lang="ts">
 	// Main mixing room components will be added here
-	import Header from './lib/Header.svelte';
-	import Home from './lib/areas/Home.svelte';
-	import { appState } from './State.svelte';
+	import Header from "./lib/Header.svelte";
+	import Home from "./lib/areas/Home.svelte";
+	import { appState } from "./State.svelte";
 </script>
 
 <Header />
 
-  {#await import(`./lib/areas/${appState.ui.area}.svelte`)}
-    <p>Loading...</p>
-  {:then module}
-    <svelte:component this={module.default} />
-  {:catch}
-    <p>Could not load view {appState.ui.area}</p>
-	<button onclick={() => appState.setView('Home')}>Go Home</button>
-  {/await}
+{#await import(`./lib/areas/${appState.ui.area}.svelte`)}
+	<p>Loading...</p>
+{:then module}
+	<svelte:component this={module.default} />
+{:catch}
+	<p>Could not load view {appState.ui.area}</p>
+	<button onclick={() => appState.setView("Home")}>Go Home</button>
+{/await}
 
 <footer id="spacer"></footer>
 
@@ -27,5 +27,4 @@
 	footer {
 		grid-area: spacer;
 	}
-
 </style>
