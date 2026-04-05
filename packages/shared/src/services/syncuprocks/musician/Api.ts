78ccd225ffd1f8263fe478a668557c44b8d758b1
pjcast <pjcast@github.com>
2026-04-04 22:30:02 -0700

    stubbing in api

---
+++ packages/shared/src/services/syncuprocks/musician/Api.ts
@@ -0,0 +1,80 @@
+import { ApiResponseBase } from "@shared/services/syncuprocks/Types";
+import { SetComplete, SetOverview } from "@shared/services/syncuprocks/musician/Types";
+import { LogError } from "@shared/services/Logger";
+
+export type Result<T, E = Error> = 
+  | { ok: true; value: T } 
+  | { ok: false; error: E };
+
+const checkHttpResponse = (response: Response, context: string) : Error | null => {
+	if (!response.ok) {
+		const errorMsg = `HTTP error ${response.status} for ${context}: ${response.statusText}`;
+		LogError(errorMsg);
+		return new Error(errorMsg);
+	}
+	
+	const contentType = response.headers.get('Content-Type');
+	if (!contentType || !contentType.includes('application/json')) {
+		const errorMsg = `Expected JSON response for ${context} but got '${contentType}'`;
+		LogError(errorMsg);
+		return new Error(errorMsg);
+	}
+
+	return null
+}
+
+const checkErrorResponse = (response: ApiResponseBase<any>, context: string, checkData: boolean) : Error | null => {
+	if (!response) {
+		const errorMsg = `No response received for ${context}`;
+		LogError(errorMsg);
+		return new Error(errorMsg);
+	} else if (!response.success) {
+		const errorMsg = `API error for ${context}: ${response.errorMessage ?? 'Unknown error'}`;
+		LogError(errorMsg);
+		return new Error(errorMsg);
+	} else if (checkData && !response.data) {
+		const errorMsg = `No data in response for ${context}`;
+		LogError(errorMsg);
+		return new Error(errorMsg);
+	}
+
+	return null;
+}
+
+/**
+ * Fetches the complete details for a specific set.
+ * @param setId Set Identifier
+ * @returns 
+ */
+export const getSetComplete = async (setId: number) : Promise<Result<SetComplete | null>> => {
+	const response = await fetch(`/api/legacy/sets/${setId}/complete`, { method: "GET", headers: { "Content-Type": "application/json" }});
+	let error = checkHttpResponse(response, `fetching complete set details for setId=${setId}`);
+	if (error)
+		return { ok: false, error };
+
+	const data: ApiResponseBase<SetComplete> = await response.json();
+	error = checkErrorResponse(data, `fetching complete set details for setId=${setId}`, true);
+	if (error)
+		return { ok: false, error };
+
+	return { ok: true, value: data.data! };
+}
+
+/**
+ * Fetches the overview list of sets for a specific musician.
+ * @param musicianId Musician Identifier
+ * @returns 
+ */
+export const getSetsOverview = async (musicianId: number) : Promise<Result<SetOverview[]>> => {
+	const response = await fetch(`/api/legacy/user/sets/overview/${musicianId}`, { method: "GET", headers: { "Content-Type": "application/json" }});
+	let error = checkHttpResponse(response, `fetching sets overview for musicianId=${musicianId}`);
+	if (error)
+		return { ok: false, error };
+
+	const data: ApiResponseBase<SetOverview[]> = await response.json();
+	error = checkErrorResponse(data, `fetching sets overview for musicianId=${musicianId}`, false);
+	if (error)
+		return { ok: false, error };
+
+	return { ok: true, value: data.data ?? [] };
+}
