d6878d1fc0f130df968e95256f10650d8f87e53f
pjcast <pjcast@github.com>
2026-05-02 22:08:28 -0700

    fix

--- apps/jam/svelte.config.js
+++ apps/jam/svelte.config.js
@@ -1,2 +1,18 @@
 /** @type {import("@sveltejs/vite-plugin-svelte").SvelteConfig} */
-export default {}
+export default {
+  // TODO ... your other config (preprocess, etc.)
+  onwarn: (warning, handler) => {
+    // List of warnings to completely ignore
+    const ignoredWarnings = [
+      'css_unused_selector',
+      'a11y_click_events_have_key_events',
+      'a11y_no_static_element_interactions',
+      'state_referenced_locally'
+    ];
+
+    if (ignoredWarnings.includes(warning.code)) return;
+
+    // For everything else, use the default handler
+    handler(warning);
+  }
+};
